e7b543b2fc2d6202cd425dd4735ab5a3e8baf655
Ketis1 <ketislahcim@gmail.com>
2024-04-18 22:02:38 +0200

    giving up fr

--- PROJEKT3/Projekt_Programistyczny_3/src/Main.java
+++ PROJEKT3/Projekt_Programistyczny_3/src/Main.java
@@ -1,5 +1,7 @@
 import java.io.File;
+import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -8,25 +10,46 @@ public class Main {
     private static List<String> testFileNames;
     public static void main(String[] args) {
 
+        trainngData=new ArrayList<>();
+        testData=new ArrayList<>();
+        String englishFolderPath = "data/training/english/";
+        String spanishFolderPath = "data/training/spanish/";
+        String polishFolderPath = "data/training/polish/";
+        loadTrainingData(englishFolderPath, "english");
+        loadTrainingData(spanishFolderPath, "spanish");
+        loadTrainingData(polishFolderPath, "polish");
+
+
 
         int epochs = 10000;
 //        Scanner scanner = new Scanner(System.in);
 //        System.out.println("podaj liczbe epok uczenia perceptronu");
 //        epochs = scanner.nextInt();
+        int numOfLetters=26;
+        Perceptron englishPerceptron = new Perceptron(0.1,numOfLetters);
+        Perceptron polishPerceptron = new Perceptron(0.1,numOfLetters);
+        Perceptron spanishPerceptron = new Perceptron(0.1,numOfLetters);
+        for (int ep=0;ep<epochs;ep++){
+            for (Observation obs :trainngData){
 
+                Collections.shuffle(trainngData);
 
-        testData=new ArrayList<>();
-        String englishFolderPath = "data/test/english/";
-        String spanishFolderPath = "data/test/spanish/";
-        String polishFolderPath = "data/test/polish/";
-        loadTestData(englishFolderPath, "english");
-        loadTestData(spanishFolderPath, "spanish");
-        loadTestData(polishFolderPath, "polish");
+                //uczenie perceptronu ktory powininen zwracac 1 jesli to jest jego jezyk
+                englishPerceptron.learn(obs.getDataFeatures(), obs.getLanguage()=="english"?1:0);
+                polishPerceptron.learn(obs.getDataFeatures(), obs.getLanguage()=="polish"?1:0);
+                spanishPerceptron.learn(obs.getDataFeatures(), obs.getLanguage()=="spanish"?1:0);
+            }
+        }
+
+        List<Perceptron> inputLayer = new ArrayList<>();
+        List<Perceptron> middleLayer = new ArrayList<>();
+        List<Perceptron> outputLayer = new ArrayList<>();
+
     }
 
 
 
-    private static List<String> loadTestFileNamesFromFolder(String folderPath) {
+    private static List<String> loadTraingFilesNamesFromFolder(String folderPath) {
         List<String> testFileNames = new ArrayList<>();
         File folder = new File(folderPath);
         File[] files = folder.listFiles();
@@ -43,11 +66,11 @@ public class Main {
         return testFileNames;
     }
 
-    private static void loadTestData(String folderPath, String language){
-        List<String> filesNames= loadTestFileNamesFromFolder(folderPath);
+    private static void loadTrainingData(String folderPath, String language){
+        List<String> filesNames= loadTraingFilesNamesFromFolder(folderPath);
         for(String fileName:filesNames){
             System.out.println(folderPath+fileName);
-            testData.add(new Observation(language,folderPath+fileName));
+            trainngData.add(new Observation(language,folderPath+fileName));
         }
     }
 }
---
+++ PROJEKT3/Projekt_Programistyczny_3/src/Perceptron.java
@@ -0,0 +1,93 @@
+import java.util.Random;
+
+public class Perceptron {
+    private double alpha;
+    private double theta;
+    private double[] weights;
+    private double bias;
+    public Perceptron(double alpha, int number_of_inputs) {
+        this.alpha = alpha;
+        theta=1;
+        weights=new double[number_of_inputs];
+        bias = Math.random() * 2 - 1;
+        for (int i=0;i< weights.length;i++){
+            weights[i]= Math.random() * 2 - 1;//random values [-1, 1]
+        }
+    }
+
+
+    public int classify(double data[]){
+        if (data.length==weights.length){
+            double sum=0;
+            for (int i=0;i<weights.length;i++){
+                sum += data[i]*weights[i];
+            }
+
+            if (sum>=theta){
+                return 1;
+            }else {
+                return 0;
+            }
+        }else{
+            System.out.println("data lenght != weights lenght");;
+            throw new IllegalArgumentException("Długość danych musi być równa długości wag");
+        }
+
+    }
+
+    public void learn(double data[],int expectedOutput){
+        int output = classify(data);
+        if (data.length==weights.length){
+
+            if (output!=expectedOutput){ //chyba mozna bez tego ale nie bedzie niepotrzebnie przez petle przechodzic
+                for (int i =0;i<weights.length;i++){
+                    weights[i]=weights[i] +(expectedOutput-output)*alpha*data[i];
+                }
+                theta = theta - (expectedOutput - output) * alpha;
+
+            }
+        }else{
+            System.out.println("data lenght != weights lenght");;
+            throw new IllegalArgumentException("Długość danych musi być równa długości wag");
+        }
+
+
+    }
+
+    public double[] getWeights() {
+        return weights;
+    }
+
+    public double getAlpha() {
+        return alpha;
+    }
+
+    public double getTheta() {
+        return theta;
+    }
+    public void getInfo(){
+        System.out.print("[");
+        for (int i = 0; i < weights.length; i++) {
+            System.out.print(weights[i]);
+            if (i < weights.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+
+    //https://www.infoworld.com/article/3685569/how-to-build-a-neural-network-in-java.html
+    public double compute(double[] input){
+        double preActivation = bias;
+        for (int i=0;i<weights.length;i++){
+            preActivation += input[i]*weights[i];
+        }
+
+        double output = sigmoid(preActivation);
+        return output;
+    }
+    public static double sigmoid(double in){
+        return 1 / (1 + Math.exp(-in));
+    }
+
+}
